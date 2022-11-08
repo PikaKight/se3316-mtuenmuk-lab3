397bfbd207ca6a996929870be3d928d56e6626ea
PikaKight <tmmarcus73@gmail.com>
2022-11-07 21:06:41 -0500

    started some mysql js code for the database

--- scripts/backend/database.js
+++ scripts/backend/database.js
@@ -1,2 +1,15 @@
 const mysql = require('mysql');
+require('dotenv').config();
+
+let con = mysql.createConnection({
+    host: process.env.HOST,
+    user: process.env.user,
+    password: process.env.password
+});
+
+con.connect(function(err) {
+    if (err) throw err;
+    console.log("Connected!");
+  });
+
 
