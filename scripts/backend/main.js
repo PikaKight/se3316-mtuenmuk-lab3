fde18d9e70a74da5f05c290ac10abf62a45d540b
PikaKight <tmmarcus73@gmail.com>
2022-11-07 20:55:25 -0500

    added a post to run my csvreader function and started making a db

--- scripts/backend/main.js
+++ scripts/backend/main.js
@@ -1,5 +1,5 @@
 const express = require("express");
-
+const csv = require("../backend/csvReader.js");
 require("dotenv").config();
 
 const app = express();
@@ -7,8 +7,16 @@ const app = express();
 app.get('/', (req, res) =>{
     console.log('here');
     res.send('hi');
-})
+});
 
+
+app.post('/api/csv', (req, res) =>{
+    
+    path = req.query.path
+    console.log(path);
+    res.send(csv.readCSV(path))
+
+});
 
 const port = process.env.PORT || 5501;
 app.listen(port, () => console.log(`listing to port ${port}`));
